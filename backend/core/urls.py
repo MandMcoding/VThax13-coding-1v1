9a13aa84e48188338d597a349ba2d3d140055072
Slumpy_666 <dr.slumpy.666@gmail.com>
2025-09-27 17:07:13 -0400

    added post logic for signp

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include  # ← include is required
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/auth/", include("authapp.urls")),  # ← points to your auth app
-]
+    path("api/", include("authapp.urls")), 
+    ]
